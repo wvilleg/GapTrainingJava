4b4444e09c5a9bf3ff96485e53c02232e5f5e462
Wendy Villegas <wendy9220@hotmail.com>
2018-03-22 00:26:33 -0600

    homeworks
    1. Regform validation
    2. screenshot

--- selenium/SeleniumBase.java
+++ selenium/SeleniumBase.java
@@ -1,30 +1,19 @@
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 
 public class SeleniumBase  {
-    String browserName;
     static WebDriver driver;
-
-    public SeleniumBase(String pname) {
-        setBrowserName(pname);
+    public SeleniumBase(WebDriver pdriver) {
+        setDriver(pdriver);
     }
+
     public SeleniumBase() {
 
-    }
-
-    public String getBrowserName() {
-        return browserName;
-    }
-
-    public void setBrowserName(String browserName) {
-        this.browserName = browserName;
     }
 
     public static WebDriver getDriver() {
@@ -35,40 +24,16 @@ public class SeleniumBase  {
         SeleniumBase.driver = driver;
     }
     
-    public String chooseYourFighter(String opt){
-        String result;
-        System.out.println("OPT " + opt);
+    public WebDriver chooseYourFighter(String opt){
+
         if (opt.contentEquals("Chrome")) {
             driver = new ChromeDriver();
-            result = "Your fighter is the mighty Chrome , hide your RAM";
+
         }else {
-            System.out.println("else");
             driver = new InternetExplorerDriver();
-            result = "Your fighter is the old and wise IE, keep the RAM but it may not work";
         }
-        initFighter();
 
-        return result;
+        return driver;
     }
 
-    public void initFighter(){
-        driver.get("http://www.google.com");
-
-        WebElement element = driver.findElement(By.name("q"));
-
-        element.sendKeys("Chrome memes");
-        element.submit();
-        sleep(3000);
-
-        driver.navigate().to("http://wanna-joke.com/wp-content/uploads/2016/07/ram-chrome-parents.jpg");
-        sleep(5000);
-        driver.quit();
     }
-    public void sleep(int pmiliseconds){
-        try {
-            Thread.sleep(pmiliseconds);
-        } catch(InterruptedException e) {
-            System.out.println("INTERRUPTION");
-        }
-    }
-}
--- selenium/Test.java
+++ selenium/Test.java
@@ -1,16 +1,56 @@
-import java.util.Scanner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Test {
 
+    public static WebDriver driver;
+
     public static void main(String[] args) {
 
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please choose your fighter wisely: Chrome or IE");
-        String answer = keyboard.nextLine();
-        SeleniumBase objBase = new SeleniumBase(answer);
-        System.out.println(objBase.chooseYourFighter(answer));
+    SeleniumBase seleniumBase = new SeleniumBase();
+    driver = seleniumBase.chooseYourFighter("Chrome");
+
+    driver.navigate().to("https://www.solarwinds.com/network-performance-monitor/registration");
+
+    driver.findElement(By.id("ci_firstName")).click();
+    driver.findElement(By.id("ci_firstName")).sendKeys("Wendy");
+    driver.findElement(By.id("ci_lastName")).sendKeys("Villegas");
+    driver.findElement(By.id("ci_company")).sendKeys("Solarwinds");
+    driver.findElement(By.id("ci_email")).sendKeys("QAAteamtestWendy@solarwinds.com");
+    driver.findElement(By.id("ci_phone")).sendKeys("12345678908");
+    driver.findElement(By.xpath("//button[@id='regFormSubmit']")).click();
+
+
+    sleep(5000);
+
+    JavascriptExecutor js = (JavascriptExecutor)driver;
+    if (js.executeScript("return document.readyState").toString().equals("complete")){
+        System.out.println("Page Is loaded.");
+    }
+
+    //sleep(10000);
+    System.out.println(driver.findElement(By.className("h1")).getText());
+    System.out.println(driver.findElement(By.xpath("//a[@href='https://support.solarwinds.com/Success_Center/Network_Performance_Monitor_%28NPM%29/Network_Performance_Monitor_Getting_Started_Guide']")).getText());
+    System.out.println(driver.findElement(By.xpath("//div[@class='col-sm-12 getting-started']//ul//li//p//a[@href='https://thwack.solarwinds.com/welcome']")).getText());
+    System.out.println(driver.findElement(By.xpath("//a[@href='/company/home']")).getText());
+
+    System.out.println(driver.findElement(By.cssSelector("div.container-fluid:nth-child(1) nav.navbar.navbar-default div.container div.collapse.navbar-collapse ul.nav.navbar-nav li.menu-toggle.separator:nth-child(5) > a.dropdown-toggle.menu-headline")).getText());
+    System.out.println(driver.findElement(By.cssSelector("div.container-fluid:nth-child(1) div.container:nth-child(2) div.row div.col-sm-8 div.container-fluid.postRegistration div.container div.row div.col-sm-8.confirmation-download > span.subheadline")).getText());
+
+    driver.quit();
 
 
 
+    }
+    public static void sleep(int pmiliseconds){
+        try {
+            Thread.sleep(pmiliseconds);
+        } catch(InterruptedException e) {
+            System.out.println("INTERRUPTION");
+        }
     }
 }
